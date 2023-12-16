ada0e08e3bcf92173c1c053b8e0665750e45b1a9
Bridger Fiore <bridger.fiore@umconnect.umt.edu>
2023-12-15 19:49:34 -0700

    Update main.js

--- Assignment_14/javascript/main.js
+++ Assignment_14/javascript/main.js
@@ -12,5 +12,9 @@ ctx.font = "30px fantasy";
 ctx.fillStyle = 'rgb(153, 153, 153)';
 ctx.fillText("We Are", 190, 50);
 
+ctx.font = "30px fantasy";
+ctx.fillStyle = 'rgb(153, 153, 153)';
+ctx.fillText("The Montana Grizzlies football (commonly referred to as the Griz) program represents the University of Montana in the Division I Football Championship Subdivision (FCS) of college football. The Grizzlies have competed in the Big Sky Conference since 1963, where it is a founding member. They play their home games on campus in Missoula at Washington–Grizzly Stadium, where they had an average attendance of 26,978 in 2023.", 200, 50);
+
 var img = document.getElementById("Grizimage");
 ctx.drawImage(img, 190, 50);
